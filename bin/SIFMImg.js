bf53dce98d0d84999e3dbe48f284785d2bb68beb
Jeff Hann <jeffhann@gmail.com>
2014-04-23 10:42:56 -0300

    fixing tab space issue

--- bin/SIFMImg.js
+++ bin/SIFMImg.js
@@ -14,9 +14,8 @@
 
   AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY({
-      region: process.env.AWS_REGION
-    })
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
   });
 
   s3 = new AWS.S3();
--- src/SIFMImg.coffee
+++ src/SIFMImg.coffee
@@ -6,7 +6,7 @@ AWS = require 'aws-sdk'
 AWS.config.update
 	accessKeyId:process.env.AWS_ACCESS_KEY_ID
 	secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
-    region:process.env.AWS_REGION
+	region:process.env.AWS_REGION
 s3 = new AWS.S3()
 
 class SIFMImg
